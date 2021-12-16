44ff0e28d149196f7aaa0437627f6c225196fa9b
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-12-16 14:18:14 +0000

    Add UI

--- Infobyte/MainWindow.Designer.cs
+++ Infobyte/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Infobyte
 {
-	partial class Messagebox
+	partial class MainWindow
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,63 +28,60 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.progressBar = new System.Windows.Forms.ProgressBar();
+			this.stop = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
-			this.Do_Nothing = new System.Windows.Forms.Button();
-			this.Notepad = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// progressBar
+			// 
+			this.progressBar.Location = new System.Drawing.Point(19, 64);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(275, 23);
+			this.progressBar.TabIndex = 0;
+			// 
+			// stop
+			// 
+			this.stop.Location = new System.Drawing.Point(300, 64);
+			this.stop.Name = "stop";
+			this.stop.Size = new System.Drawing.Size(75, 23);
+			this.stop.TabIndex = 1;
+			this.stop.Text = "Stop";
+			this.stop.UseVisualStyleBackColor = true;
+			this.stop.Click += new System.EventHandler(this.stop_Click);
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Location = new System.Drawing.Point(13, 13);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(115, 31);
-			this.label1.TabIndex = 0;
+			this.label1.TabIndex = 2;
 			this.label1.Text = "InfoByte";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(18, 44);
+			this.label2.Location = new System.Drawing.Point(19, 48);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(306, 13);
-			this.label2.TabIndex = 1;
-			this.label2.Text = "Do you want to open the results in notepad or copy to clipboard";
+			this.label2.Size = new System.Drawing.Size(275, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Infobyte is currently diagnosing your system please wait...\r\n";
 			// 
-			// Do_Nothing
-			// 
-			this.Do_Nothing.Location = new System.Drawing.Point(249, 62);
-			this.Do_Nothing.Name = "Do_Nothing";
-			this.Do_Nothing.Size = new System.Drawing.Size(75, 23);
-			this.Do_Nothing.TabIndex = 2;
-			this.Do_Nothing.Text = "Do nothing";
-			this.Do_Nothing.UseVisualStyleBackColor = true;
-			this.Do_Nothing.Click += new System.EventHandler(this.Close_Click);
-			// 
-			// Notepad
-			// 
-			this.Notepad.Location = new System.Drawing.Point(143, 62);
-			this.Notepad.Name = "Notepad";
-			this.Notepad.Size = new System.Drawing.Size(100, 23);
-			this.Notepad.TabIndex = 3;
-			this.Notepad.Text = "Open in Notepad";
-			this.Notepad.UseVisualStyleBackColor = true;
-			this.Notepad.Click += new System.EventHandler(this.Notepad_Click);
-			// 
-			// Messagebox
+			// MainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(329, 97);
-			this.Controls.Add(this.Notepad);
-			this.Controls.Add(this.Do_Nothing);
+			this.ClientSize = new System.Drawing.Size(388, 97);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
-			this.Name = "Messagebox";
-			this.Text = "Form1";
+			this.Controls.Add(this.stop);
+			this.Controls.Add(this.progressBar);
+			this.Name = "MainWindow";
+			this.Text = "MainWindow";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -92,9 +89,9 @@
 
 		#endregion
 
+		private System.Windows.Forms.ProgressBar progressBar;
+		private System.Windows.Forms.Button stop;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Button Do_Nothing;
-		private System.Windows.Forms.Button Notepad;
 	}
 }
---
+++ Infobyte/MainWindow.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Security.Principal;
+using System.Reflection;
+using System.Threading.Tasks;
+namespace Infobyte
+{
+	public partial class MainWindow : Form
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			DateTime timeofstart = DateTime.UtcNow;
+			Console.WriteLine("Starting Diagnostics...");
+			if (IsElevated == false)
+			{
+				Console.WriteLine("WARNING: InfoByte Is not elevated please run the program as a admin");
+			}
+			// Cpu Usage
+			var cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+			Thread.Sleep(10);
+			var firstCall = cpuUsage.NextValue();
+			Thread.Sleep(10);
+			var cpu = cpuUsage.NextValue() + "%";
+			progressBar.Value = 10;
+			//Processes
+			Process[] processlist = Process.GetProcesses();
+			File.WriteAllText("SystemInfo.txt", "InfoByte Diagnostics:");
+			File.AppendAllText("SystemInfo.txt", Environment.NewLine + "Time of Diagnostics: " + timeofstart.ToString());
+			File.AppendAllText("SystemInfo.txt", Environment.NewLine + "Machine Name: " + Environment.MachineName);
+
+			File.AppendAllText("SystemInfo.txt", Environment.NewLine + "CPU at the time of the test: " + cpu);
+			File.AppendAllText("SystemInfo.txt", Environment.NewLine + "===================================");
+			progressBar.Value = 20;
+			foreach (Process process in processlist)
+			{
+				File.AppendAllText("SystemInfo.txt", Environment.NewLine + "Process |" + process.ProcessName);
+			}
+			progressBar.Value = 40;
+			File.AppendAllText("SystemInfo.txt", Environment.NewLine + "===================================");
+			//Drive Info
+			DriveInfo[] allDrives = DriveInfo.GetDrives();
+			progressBar.Value = 100;
+			foreach (DriveInfo d in allDrives)
+			{
+				File.AppendAllText("SystemInfo.txt", Environment.NewLine + "Drive " + d.Name);
+				File.AppendAllText("SystemInfo.txt", Environment.NewLine + "  Drive type: " + d.DriveType);
+				if (d.IsReady == true)
+				{
+					File.AppendAllText("SystemInfo.txt", Environment.NewLine + "  Volume label:" + d.VolumeLabel);
+					File.AppendAllText("SystemInfo.txt", Environment.NewLine + "  File system: " + d.DriveFormat);
+					File.AppendAllText("SystemInfo.txt", Environment.NewLine + "  Available space to current user: " + d.AvailableFreeSpace + " Bytes");
+					File.AppendAllText("SystemInfo.txt", Environment.NewLine + "  Total available space:" + d.TotalFreeSpace + " Bytes");
+					File.AppendAllText("SystemInfo.txt", Environment.NewLine + "  Total size of drive:" + d.TotalSize + " Bytes");
+				}
+			}
+			Messagebox msg = new Messagebox();
+			msg.Show();
+			this.Hide();
+		}
+
+		private void stop_Click(object sender, EventArgs e)
+		{
+			string message = "Do you want to close this window? Doing this will stop the test";
+			string title = "Close Window";
+			MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+			DialogResult result = MessageBox.Show(message, title, buttons);
+			if (result == DialogResult.Yes)
+			{
+				Environment.Exit(0);
+			}
+			else
+			{
+				;
+			}
+		}
+		static bool IsElevated
+		{
+			get
+			{
+				return WindowsIdentity.GetCurrent().Owner
+				  .IsWellKnown(WellKnownSidType.BuiltinAdministratorsSid);
+			}
+		}
+	}
+}
